1706094b08680d9f904c6b8d8e01eca3a2283951
jhanvi006 <jhanvikankhara06@gmail.com>
2022-07-14 23:15:39 +0530

    Added: environment testing is added.

--- src/main/java/com/bridgelabz/EmployeePayrollApp/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/EmployeePayrollApp/EmployeePayrollAppApplication.java
@@ -1,15 +1,19 @@
 package com.bridgelabz.EmployeePayrollApp;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @Slf4j
 public class EmployeePayrollAppApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeePayrollAppApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(EmployeePayrollAppApplication.class, args);
 		log.info("Welcome to Employee Payroll App.");
+		log.info("Employee Payroll App started in {} Environment",
+				context.getEnvironment().getProperty("environment"));
 	}
 
 }
