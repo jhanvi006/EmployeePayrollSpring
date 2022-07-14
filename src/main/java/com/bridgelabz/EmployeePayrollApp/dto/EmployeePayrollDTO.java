f4e6df77b74af3067b02b5ffd02bb21620ca4dfd
jhanvi006 <jhanvikankhara06@gmail.com>
2022-07-14 22:51:49 +0530

    Added: lombok annotations are used.

--- src/main/java/com/bridgelabz/EmployeePayrollApp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/EmployeePayrollApp/dto/EmployeePayrollDTO.java
@@ -1,18 +1,11 @@
 package com.bridgelabz.EmployeePayrollApp.dto;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class EmployeePayrollDTO {
     public String name;
     public String department;
     public String gender;
     public long salary;
-
-    public EmployeePayrollDTO(String name, String department, String gender, long salary){
-        this.name=name;
-        this.department=department;
-        this.gender=gender;
-        this.salary=salary;
-    }
-    public String toString(){
-        return "name: "+name+", department: "+department+", gender: "+gender+", salary: "+salary;
-    }
 }
--- src/main/java/com/bridgelabz/EmployeePayrollApp/model/Employee.java
+++ src/main/java/com/bridgelabz/EmployeePayrollApp/model/Employee.java
@@ -1,18 +1,23 @@
 package com.bridgelabz.EmployeePayrollApp.model;
 
 import com.bridgelabz.EmployeePayrollApp.dto.EmployeePayrollDTO;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+@Data
+@NoArgsConstructor
 @Entity
 public class Employee {
+    @Id
+    @GeneratedValue
     private int employee_id;
     private String name, department, gender;
     private long salary;
 
-    public Employee(){}
     public Employee(int id, EmployeePayrollDTO employeePayrollDTO){
         this.employee_id=id;
         this.name=employeePayrollDTO.name;
@@ -20,46 +25,6 @@ public class Employee {
         this.gender=employeePayrollDTO.gender;
         this.salary= employeePayrollDTO.salary;
     }
-
-    @Id
-    @GeneratedValue
-    public long getEmployee_id() {
-        return employee_id;
-    }
-
-    public void setEmployee_id(int employee_id) {
-        this.employee_id = employee_id;
-    }
-
-    public long getSalary() {
-        return salary;
-    }
-
-    public void setSalary(long salary) {
-        this.salary = salary;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
 }
+
+
